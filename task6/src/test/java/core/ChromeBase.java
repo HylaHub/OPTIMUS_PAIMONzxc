2f80efbd74a8baf1da965b892623dc393baaa8c8
NikitosHeh <149762482+NikitosHeh@users.noreply.github.com>
2025-02-15 15:41:41 +0300

    Add files via upload

---
+++ task6/src/test/java/core/ChromeBase.java
@@ -0,0 +1,27 @@
+package core;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+
+public class ChromeBase {
+    protected WebDriver driver;
+
+    @BeforeEach
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+    }
+
+    @AfterEach
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
