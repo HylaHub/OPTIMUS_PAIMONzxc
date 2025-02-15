f88d16855ce3d6ecdc72583b8e20eee7abee77b9
NikitosHeh <149762482+NikitosHeh@users.noreply.github.com>
2025-02-15 17:03:40 +0300

    Update ChromeBase.java

--- task6/src/test/java/core/ChromeBase.java
+++ task6/src/test/java/core/ChromeBase.java
@@ -1,27 +1,33 @@
 package core;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.WebDriver;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.*;
+
 import java.time.Duration;
 
 public class ChromeBase {
+
     protected WebDriver driver;
+    protected WebDriverWait wait;
 
     @BeforeEach
     public void setup() {
-        WebDriverManager.chromedriver().setup();
+        // Инициализация WebDriver
+        System.setProperty("webdriver.chrome.driver", "C:/selenium/chromedriver.exe");
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5)); // Для явных ожиданий
         driver.manage().window().maximize();
+
+        // Инициализация WebDriverWait
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
     }
 
     @AfterEach
     public void teardown() {
         if (driver != null) {
-            driver.quit();
+            driver.quit(); // Закрытие браузера после каждого теста
         }
     }
 }
